9918063dd9eaf81c8df6893f96fd7208b6524729
Carlos Capitao <cccd1520@gmail.com>
2025-09-02 22:20:38 +0100

    inicio

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HearoArea from '@/Layouts/HearoArea.vue';
+import Footer from '@/Layouts/Footer.vue';
+import Section from '@/Pages/Section.vue';
+import { Head, Link } from '@inertiajs/vue3';
+
+
+defineProps({
+
+});
+</script>
+
+<template>
+
+    <Head title="Home" />
+    <HearoArea />
+    <Section />
+     <!-- end client section -->
+    <Footer />
+</template>
